af080da2d5c2b633607edfd526ff847a0744cc0a
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-10 14:28:18 -0400

    1.0.3

--- obb.js
+++ obb.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/public/styles', function (req, res) {
+    res.sendFile('/obb/public/styles/index.css', {root: __dirname});
+});
+
 app.get('/', function (req, res) {
-    res.sendFile('/obb/content/index.html', {root: __dirname});
+    res.sendFile('/obb/index.html', {root: __dirname});
 });
 
 app.listen(3000);
