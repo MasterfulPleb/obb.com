422359ed22cbfe5b3a57609aa18b7840a9b89af2
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-10 17:01:14 -0400

    1.0.9

--- obb.js
+++ obb.js
@@ -1,14 +1,14 @@
 'use strict';
 const express = require('express');
 const app = express();
+const pug = require('pug');
 
 app.set('view engine', 'pug');
 
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Derp' })
-    //res.sendFile('/obb/index.html', {root: __dirname});
+    res.render('index', { message: 'Derp' });
 });
 
 app.get('*', (req, res) => res.redirect('/'));
