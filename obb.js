8522b1e02ce241e84a0bd760e2f40dda3d70bce8
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-11 16:23:46 -0400

    1.0.21

--- obb.js
+++ obb.js
@@ -2,27 +2,71 @@
 const express = require('express');
 const app = express();
 const pug = require('pug');
-const mariadb = require('mariadb')
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+        socketPath: '/var/run/mysqld/mysqld.sock',
+        user: 'root',
+        database: 'bee_movie',
+        connectionLimit: 5,
+});
 
 app.set('view engine', 'pug');
 
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-    mariadb.createConnection({
-        socketPath: '/var/run/mysqld/mysqld.sock',
-        user: 'root',
-        database: 'bee_movie'
-    })
-    .then(conn => {
-        conn.query('SELECT author, COUNT(2) AS "totalComments"' +
-            ' FROM comments GROUP BY author ORDER BY COUNT(2) DESC;')
-        .then(arr => res.render('index', { array: arr }))
-        .catch(err => console.error('mariadb query error: ' + err));
-    })
-    .catch(err => console.error('mariadb connection error: ' + err));
+    pool.query('SELECT author, ' +
+        'COUNT(*) AS "comments" FROM comments ' +
+        'GROUP BY author ORDER BY COUNT(*) DESC;')
+    .then(arr => res.render('index', { leaderboard: arr }))
+    .catch(err => console.error('mariadb error: ' + err));
 });
 
 app.get('*', (req, res) => res.redirect('/'));
 
 app.listen(3000);
+
+
+/*  //let conn;
+    try {
+        conn = await pool.getConnection();
+        const leaderboard = await conn.query('SELECT author, ' +
+            'COUNT(*) AS "comments" FROM comments ' +
+            'GROUP BY author ORDER BY COUNT(*) DESC;'
+        );
+        res.render('index', { leaderboard: leaderboard })
+    } catch (err) {
+        console.error('mariadb connection error: ' + err)
+    } finally {
+        if (conn) return conn.end()
+    }
+
+
+
+
+    //let leaderboard;
+    pool.getConnection()
+        .then(conn => {
+            conn.query('SELECT author, ' +
+                'COUNT(*) AS "comments" FROM comments ' +
+                'GROUP BY author ORDER BY COUNT(*) DESC;')
+            .then(arr => leaderboard = arr)
+            .finally(() => conn.release)
+        })
+        .then(arr => {
+            res.render('index', { leaderboard: arr })
+            
+        })
+        .catch(err => console.error('mariadb error: ' + err))
+
+
+
+
+    pool.getConnection()
+    .then(conn => {
+        conn.query('SELECT author, COUNT(*) AS "comments" ' +
+            'FROM comments GROUP BY author ORDER BY COUNT(*) DESC;')
+        .then(arr => res.render('index', { array: arr }))
+        .catch(err => console.error('mariadb query error: ' + err));
+    })
+    .catch(err => console.error('mariadb connection error: ' + err));*/
