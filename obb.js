cea7f4c4f2454f5f95410b490195b7e6dbb8c123
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-15 14:50:50 -0400

    querys

--- obb.js
+++ obb.js
@@ -17,14 +17,19 @@ app.use(favicon('./public/favicon.ico'));
 
 app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => {
-    pool.query('SELECT author, ' +
-        'COUNT(*) AS "comments" FROM comments ' +
-        'GROUP BY author ORDER BY COUNT(*) DESC;')
-    .then(arr => {
-        res.render('index', { leaderboard: arr });
-    })
-    .catch(err => console.error('mariadb query error: ' + err));
+app.get('/', async function (req, res) {
+    const conn = await pool.getConnection();
+    try {
+        var leaderboard = await conn.query('SELECT author, ' +
+            'COUNT(*) AS "comments" FROM comments ' +
+            'GROUP BY author ORDER BY COUNT(*) DESC;');
+        var lastCommentPerma = await conn.query('SELECT permalink ' +
+            'FROM comments ORDER BY timestamp DESC LIMIT 1;');
+        lastCommentURL = 'https://www.reddit.com' + lastCommentPerma + '?context=3';
+    } catch (err) {
+        console.error('mariadb query error: ' + err);
+    }
+    res.render('index', { leaderboard: leaderboard, lastCommentURL: lastCommentURL });
 });
 
 app.get('*', (req, res) => res.redirect('/'));
