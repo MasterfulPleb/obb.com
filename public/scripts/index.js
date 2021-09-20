2ad1ffb0d82259dbc397e78a56f4abe9748be853
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-20 00:50:53 -0400

    websocket

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,7 +2,7 @@
 
 const WebSocket = require("ws");
 
-let ws = new WebSocket('wss://test.ouijabeederboard.com/ws')
+let ws = new WebSocket('wss://test.ouijabeederboard.com/ws');
 
 ws.onopen = (e) => {
     console.log('websocket connected')
