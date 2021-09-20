4f3c29cc1dd60dda61b7f43ef3ee181f671f2074
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-20 02:21:11 -0400

    websocket

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,10 +3,26 @@
 let ws = new WebSocket('wss://test.ouijabeederboard.com/ws');
 
 ws.onopen = (ev) => {
-    console.log('websocket connected')
+    console.log('websocket connected');
 };
-
 ws.onmessage = (msg) => {
-    let data = JSON.parse(msg.data)
+    const data = JSON.parse(msg.data)
     console.log(data)
+    updatePage(data)
+}
+
+function updatePage(data) {
+    const written = document.getElementById('written')
+    const remaining = document.getElementById('remaining')
+    const oldLength = written.innerHTML.length
+    const newLength = data.progress
+    const diff = newLength - oldLength
+    if (diff > 0) {
+        written.innerHTML += remaining.innerHTML.slice(0, diff)
+        remaining.innerHTML = remaining.innerHTML.slice(diff)
+        document.getElementById('percent').innerHTML = data.percent
+        document.getElementById('percent24').innerHTML = data.percent24
+        document.getElementById('last-written').innerHTML = data.lastWritten
+        document.getElementById('first-remaining').innerHTML = data.firstRemaining
+    }
 }
