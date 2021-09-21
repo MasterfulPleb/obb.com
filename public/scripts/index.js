055b86fc77729f93c297860297889d4af7decf68
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-20 22:16:10 -0400

    websocket

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -21,7 +21,7 @@ function updatePage(data) {
     const oldLength = written.innerHTML.length;
     const newLength = data.progress;
     const diff = newLength - oldLength;
-    console.log(diff + ' new comment' + (diff > 1 ? 's' : '') + ', updating page');
+    console.log(diff + ' new comment' + (diff == 1 ? '' : 's') + ', updating page');
     written.innerHTML += remaining.innerHTML.slice(0, diff);
     remaining.innerHTML = remaining.innerHTML.slice(diff);
     document.getElementById('percent').innerHTML = data.percent;
@@ -30,5 +30,16 @@ function updatePage(data) {
     document.getElementById('first-remaining').innerHTML = data.firstRemaining;
     for (let update of data.leaderboard) {
         document.getElementById(update.author).children[2].innerHTML = update.comments
+    }
+    const leaderboard = document.getElementById('leaderboard')
+    for (let i = 0; i < leaderboard.children.length - 1; i++) {
+        let score = parseInt(leaderboard.children[i].children[2].innerHTML)
+        let nextScore = parseInt(leaderboard.children[i + 1].children[2].innerHTML)
+        if (score < nextScore) {
+            let t = leaderboard.children[i].children[0].innerHTML
+            leaderboard.children[i].children[0].innerHTML = leaderboard.children[i+1].children[0].innerHTML
+            leaderboard.children[i+1].children[0].innerHTML = t
+            leaderboard.insertBefore(leaderboard.children[i+1], leaderboard.children[i])
+        }
     }
 }
