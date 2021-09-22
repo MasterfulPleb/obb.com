d744d4f77117a7b1187efffaa959b390e16af4dd
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-21 21:01:39 -0400

    charts

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', async function() {
-    let data = await fetch('https://test.ouijabeederboard.com/charts/commentsPie')
-    const chart = Highcharts.chart('chart', JSON.parse(data.body));
+    let data = await fetch('https://test.ouijabeederboard.com/charts/commentsPie');
+    console.log(data);
+    const chart = Highcharts.chart('chart', data);
 });
