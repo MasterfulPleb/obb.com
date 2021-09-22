088fc9ce045312ccb9b8f423bec53ed66f0b514e
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-21 20:50:58 -0400

    charts

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -1,31 +1,6 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const chart = Highcharts.chart('chart', {
-        chart: {
-            type: 'pie'
-        },
-        title: {
-            text: 'Comments per user'
-        },
-        series: [{
-            name: 'someone',
-            data: [{
-                name: 'someone',
-                y: 1234
-            }, {
-                name: 'someone else',
-                y: 3456
-            }, {
-                name: 'ligma',
-                y: 2345
-            }, {
-                name: 'joe',
-                y: 6879
-            }, {
-                name: 'jack mehoff',
-                y: 1456
-            }]
-        }]
-    });
+document.addEventListener('DOMContentLoaded', async function() {
+    let data = await fetch('https://test.ouijabeederboard.com/charts/commentsPie')
+    const chart = Highcharts.chart('chart', data);
 });
