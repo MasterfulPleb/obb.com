4eec94e39c6034afcef146a2f6d41af49a1cea16
MasterfulPleb <justinmunger027@yahoo.com>
2021-09-23 18:30:52 -0400

    charts

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -1,10 +1,10 @@
 'use strict';
 
 let fetchChart = fetch('https://test.ouijabeederboard.com/charts/commentsPie')
+    .then(res => res.json())
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchChart
-      .then(res => res.json())
       .then(data => {
         console.log(data);
         const chart = Highcharts.chart('chart', data);
