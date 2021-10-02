6296989bd4f6c7656aea9b98299ddd2a8a3d61fd
MasterfulPleb <justinmunger027@yahoo.com>
2021-10-02 01:58:33 -0400

    b

--- obb.js
+++ obb.js
@@ -304,30 +304,7 @@ var commentsHeat = {
         marginBottom: 80,
         plotBorderWidth: 1,
         backgroundColor: '#282828',
-        events: {
-            drilldown: function (e) {
-                var chart = this;
-                chart.yAxis[0].update({
-                    type: 'linear',
-                    categories: false
-                });
-                chart.xAxis[0].update({
-                    type: 'linear',
-                    categories: false
-                });
-            },
-            drillup: function (e) {
-                var chart = this;
-                chart.yAxis[0].update({
-                    type: 'category',
-                    categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-                });
-                chart.xAxis[0].update({
-                    type: 'category',
-                    categories: ['7/4','7/11','7/18','7/25','8/1','8/8','8/15','8/22','8/29','9/5','9/12','9/19','9/26']
-                });
-            }
-        }
+        events: {}
     },
     title: {
         text: 'Comments per day',
